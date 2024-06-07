260f907cf151e731a70f56cfaf4c9e8aa1dd3724
nbrosmum <103727624+nbrosmum@users.noreply.github.com>
2024-06-07 13:42:29 +0800

    header done

--- app.js
+++ app.js
@@ -7,21 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 
@@ -37,6 +22,25 @@ db.connect((err) => {
     } else {
         console.log('Connected to the database');
     }
+});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
 // catch 404 and forward to error handler
