63265c725eb1f86978d7bf59add2de3ab06c68d4
nbrosmum <103727624+nbrosmum@users.noreply.github.com>
2024-06-08 11:05:03 +0800

    Index page done

--- app.js
+++ app.js
@@ -39,9 +39,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
